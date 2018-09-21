fe655640b1d6fa7e955ad28e6d3716904bc7dd2f
Miles <miles.b.flo@gmail.com>
2018-09-20 23:29:34 -0700

    Implemented winner calculation

--- src/index.js
+++ src/index.js
@@ -38,6 +38,9 @@ class Board extends React.Component {
     // It is also more performant in React to "batch update" an object... Only one traversal
     // of the DOM is required to see these changes.
     const squares = this.state.squares.slice();
+    if (calculateWinner(squares) || squares[i]) {
+      return;
+    }
     squares[i] = this.state.xIsNext ? 'X' : 'O';
     this.setState({
       squares: squares,
@@ -59,7 +62,14 @@ class Board extends React.Component {
   }
 
   render() {
-    const status = `Next player: ${this.state.xIsNext ? 'X' : 'O'}`;
+    const winner = calculateWinner(this.state.squares);
+    let status;
+    if (winner) {
+      status = 'Winner: ' + winner;
+    } else {
+      status = `Next player: ${this.state.xIsNext ? 'X' : 'O'}`;
+    }
+    
 
     return (
       <div>
@@ -106,3 +116,24 @@ ReactDOM.render(
   <Game />,
   document.getElementById('root')
 );
+
+// This is absurd and I'm glad it came with the tute
+function calculateWinner(squares) {
+  const lines = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6],
+  ];
+  for (let i = 0; i < lines.length; i++) {
+    const [a, b, c] = lines[i];
+    if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
+      return squares[a];
+    }
+  }
+  return null;
+}
