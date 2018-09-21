f64e465383cf45825669b692e8fe98f73dfbc1e1
Miles <miles.b.flo@gmail.com>
2018-09-20 23:51:34 -0700

    Lifting up state management from Board to Game class

--- src/index.js
+++ src/index.js
@@ -15,65 +15,23 @@ function Square(props) {
 }
 
 class Board extends React.Component {
-  /* 
-    In React, the parent should be responsible for maintaining the state of all children,
-    not each child maintaining their own state. State can be passed from parent to child
-    using `props`. This is more efficient, reliable, and maintainable.
-
-  */
-  constructor(props) {
-    // `super` is a special function when working with classes, and more specifically
-    // subclasses. Super will inherit the methods and relevant properties of the
-    // parent class.. In this case `React.Component`
-    super(props);
-    this.state = {
-      squares: Array(9).fill(null),
-      xIsNext: true,
-    }
-  }
-
-  handleClick(i) {
-    // Sliced to change data without mutation. We want to preserve the state of `squares`
-    // at this point in time so that we can have an "undo" button
-    // It is also more performant in React to "batch update" an object... Only one traversal
-    // of the DOM is required to see these changes.
-    const squares = this.state.squares.slice();
-    if (calculateWinner(squares) || squares[i]) {
-      return;
-    }
-    squares[i] = this.state.xIsNext ? 'X' : 'O';
-    this.setState({
-      squares: squares,
-      xIsNext: !this.state.xIsNext,
-    })
-  }
-
   renderSquare(i) {
     return <Square
-      value={this.state.squares[i]}
+      value={this.props.squares[i]}
       /*
         State can't be updated from child up to parent, but you _can_ pass functions
         as a prop. functions will be executed from the scope of their definition, in
         this case, Board. On Square click, it will execute the `handleClick` method in
         the parent Board.
       */
-      onClick={() => this.handleClick(i)}
+      onClick={() => this.props.onClick(i)}
     />;
   }
 
   render() {
-    const winner = calculateWinner(this.state.squares);
-    let status;
-    if (winner) {
-      status = 'Winner: ' + winner;
-    } else {
-      status = `Next player: ${this.state.xIsNext ? 'X' : 'O'}`;
-    }
-    
-
+  
     return (
       <div>
-        <div className="status">{status}</div>
         <div className="board-row">
           {this.renderSquare(0)}
           {this.renderSquare(1)}
@@ -95,19 +53,68 @@ class Board extends React.Component {
 }
 
 class Game extends React.Component {
+  /* 
+    In React, the parent should be responsible for maintaining the state of all children,
+    not each child maintaining their own state. State can be passed from parent to child
+    using `props`. This is more efficient, reliable, and maintainable.
+  */
+   
+  constructor(props) {
+    // `super` is a special function when working with classes, and more specifically
+    // subclasses. Super will inherit the methods and relevant properties of the
+    // parent class.. In this case `React.Component`
+    super(props);
+    this.state = {
+      history: [{
+        squares: Array(9).fill(null),
+      }],
+      xIsNext: true,
+    }
+  }
   render() {
+    const history = this.state.history;
+    const current = history[history.length - 1];
+    const winner = calculateWinner(current.squares);
+    let status;
+    if (winner) {
+      status = 'Winner: ' + winner;
+    } else {
+      status = 'Next player: ' + (this.state.xIsNext ? 'X' : 'O');
+    }
+
     return (
       <div className="game">
         <div className="game-board">
-          <Board />
+          <Board squares={current.squares} onClick={(i) => this.handleClick(i)}/>
         </div>
         <div className="game-info">
-          <div>{/* status */}</div>
+          <div>{ status }</div>
           <ol>{/* TODO */}</ol>
         </div>
       </div>
     );
   }
+  handleClick(i) {
+    const history = this.state.history;
+    const current = history[history.length - 1];
+    // Sliced to change data without mutation. We want to preserve the state of `squares`
+    // at this point in time so that we can have an "undo" button
+    // It is also more performant in React to "batch update" an object... Only one traversal
+    // of the DOM is required to see these changes.
+    const squares = current.squares.slice();
+    if (calculateWinner(squares) || squares[i]) {
+      return;
+    }
+    squares[i] = this.state.xIsNext ? 'X' : 'O';
+    this.setState({
+      // Using `concat` here is prefered over `push` since it does not mutate the original array
+      history: history.concat([{
+        squares: squares,
+      }]),
+      xIsNext: !this.state.xIsNext,
+    })
+  }
+
 }
 
 // ========================================
